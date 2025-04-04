652973fff5c0ae6cd011f1f13c35e5b42bd2710f
Artem Lazarenko <deadser1989@gmail.com>
2025-04-05 00:37:55 +0300

    обьявил вроде бы все нужные функции

--- labaPolymorph/typeinfo.h
+++ labaPolymorph/typeinfo.h
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 
 typedef void (*binaryOperator)(const void* a, const void* b, void* result);
-typedef size_t(*getSize)(void* destination,const void* source);
+typedef void (*assignment)(void* destination,const void* source);
+typedef size_t(*getSize)();
+typedef void (*print)(const void* element);
+typedef void (*destroy)(void* element);
+typedef void (*swapElements)(void* element1, void* element2);
+
 
 typedef struct typeInfo{
 	binaryOperator add;
