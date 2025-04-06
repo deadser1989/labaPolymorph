abd49299f320321aea238e80c116da6e4b51e379
Artem Lazarenko <deadser1989@gmail.com>
2025-04-06 18:30:32 +0300

    integer доделал

--- labaPolymorph/integer.c
+++ labaPolymorph/integer.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 void intAdd(const void* a, const void* b, void* res) {
-	*(int*)res = (*(const int*) a + *(const int*) b);
+	*(int*)res = (*(const int*) a + *(const int*) b); //значение на которое указывает res
 }
 
 void intMultiply(const void* a, const void* b, void* res) {
@@ -21,4 +21,31 @@ void intSwap(void* element1, void* element2) {
 	int temp = *(int*)element1;
 	*(int*)element1 = *(int*)element2;
 	*(int*)element2 = temp;
+}
+
+size_t intGetSize(void) {
+	return sizeof(int);
+}
+
+void intPrint(const void* element) {
+	printf("%d", *(const int*)element);
+}
+
+void freeInteger(void* ptr)
+{
+	free(ptr);
+}
+
+const TypeInfo* getTypeInfoInteger(void) {
+	static const TypeInfo integerTypeInfo = {
+		.add = intAdd,
+		.multiply = intMultiply,
+		.substract = intSubstract,
+		.assign = intAssign,
+		.swap = intSwap,
+		.size = intGetSize,
+		.print = intPrint,
+		.destroy = freeInteger
+	};
+	return &integerTypeInfo;
 }
