4b3016a9507fb4dde4e16007636a59852ba5cbc1
Artem Lazarenko <deadser1989@gmail.com>
2025-04-13 18:11:47 +0300

    complex commands add

--- labaPolymorph/complex.c
+++ labaPolymorph/complex.c
@@ -3,9 +3,24 @@
 #include <stdlib.h>
 
 void complexAdd(const void* a, const void* b, void* res) {
-	complex* result = (complex*)res;
-	const complex* num1 = (const complex*)a;
-	const complex* num2 = (const complex*)b;   
-	result->real = num1->real + num2->real;
-	result->imag = num1->imag + num2->imag;
+	complex* complexRes = (complex*)res;
+	const complex* complexA = (const complex*)a;
+	const complex* complexB = (const complex*)b;   
+	complexRes->real = complexA->real + complexB->real;
+	complexRes->imag = complexA->imag + complexB->imag;
+}
+
+void complexSubstract(const void* a, const void* b, void* res) {
+	complex* complexRes = (complex*)res;
+	const complex* complexA = (const complex*)a;
+	const complex* complexB = (const complex*)b;
+	complexRes->real = complexA->real - complexB->real;
+	complexRes->imag = complexA->imag - complexB->imag;
+}
+
+void complexMultiply(const void* a, const void* b, void* res) {
+	complex* complexRes = (complex*)res;
+	const complex* complexA = (const complex*)a;
+	const complex* complexB = (const complex*)b;
+
 }
